07c63dbbbc050f75515ebcb70b9d7eff80185922
Ilia Barulin <ibarulin@york.citycollege.eu>
2026-01-07 21:26:45 +0300

    server menu for pausing and resuming added

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -25,20 +27,62 @@ func main() {
 		return
 	}
 	defer amqpCh.Close()
-	err = pubsub.PublishJSON(
-		amqpCh,
-		routing.ExchangePerilDirect,
-		routing.PauseKey,
-		routing.PlayingState{IsPaused: true},
-	)
+	/*
+		err = pubsub.PublishJSON(
+			amqpCh,
+			routing.ExchangePerilDirect,
+			routing.PauseKey,
+			routing.PlayingState{IsPaused: true},
+		)
+	*/
 
 	fmt.Println("AMQP connection was successful.")
 	fmt.Println("Starting Peril server...")
 
-	sigCh := make(chan os.Signal, 1)
+	gamelogic.PrintServerHelp()
 
+loop:
+	for {
+		input := gamelogic.GetInput()
+		if len(input) == 0 {
+			continue
+		}
+
+		switch input[0] {
+		case "pause":
+			fmt.Println("Sending pause message")
+			err = pubsub.PublishJSON(
+				amqpCh,
+				string(routing.ExchangePerilDirect),
+				string(routing.PauseKey),
+				routing.PlayingState{IsPaused: true},
+			)
+			if err != nil {
+				log.Printf("could not publish: %v", err)
+			}
+		case "resume":
+			fmt.Println("Sending pause message")
+			err = pubsub.PublishJSON(
+				amqpCh,
+				string(routing.ExchangePerilDirect),
+				string(routing.PauseKey),
+				routing.PlayingState{IsPaused: false},
+			)
+			if err != nil {
+				log.Printf("could not publish: %v", err)
+			}
+		case "quit":
+			fmt.Println("Exiting loop because of quit command")
+			break loop
+		default:
+			fmt.Println("unknown command")
+		}
+	}
+
+	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
 	<-sigCh
+
 	fmt.Println("Shutting down Peril server...")
 
 }
