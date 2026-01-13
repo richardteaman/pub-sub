a69e5b70158e35c2f4c529bde7eef5cacead1806
Ilia Barulin <ibarulin@york.citycollege.eu>
2026-01-14 02:39:23 +0300

    added move handle for client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -16,6 +16,13 @@ func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) {
 	}
 }
 
+func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) {
+	return func(move gamelogic.ArmyMove) {
+		defer fmt.Print("> ")
+		gs.HandleMove(move)
+	}
+}
+
 func main() {
 	amqpUrl := "amqp://guest:guest@localhost:5672/"
 	amqpCon, err := amqp.Dial(amqpUrl)
@@ -53,6 +60,27 @@ func main() {
 		handlerPause(gameState),
 	)
 
+	moveQueueName := fmt.Sprintf("%s.%s", routing.ArmyMovesPrefix, username)
+	moveKey := fmt.Sprintf("%s.*", routing.ArmyMovesPrefix)
+
+	err = pubsub.SubscribeJSON(
+		amqpCon,
+		routing.ExchangePerilTopic,
+		moveQueueName,
+		moveKey,
+		pubsub.QueueTransient,
+		handlerMove(gameState),
+	)
+	if err != nil {
+		return
+	}
+
+	pubCh, err := amqpCon.Channel()
+	if err != nil {
+		return
+	}
+	defer pubCh.Close()
+
 	fmt.Println("Starting Peril client...")
 
 loop:
@@ -69,11 +97,24 @@ loop:
 				fmt.Println(err)
 			}
 		case "move":
-			_, err = gameState.CommandMove(input)
+			move, err := gameState.CommandMove(input)
 			if err != nil {
 				fmt.Println(err)
 			} else {
 				fmt.Println("Move successful")
+				moveKey := fmt.Sprintf("%s.%s", routing.ArmyMovesPrefix, username)
+				err = pubsub.PublishJSON(
+					pubCh,
+					routing.ExchangePerilTopic,
+					moveKey,
+					move,
+				)
+				if err != nil {
+					fmt.Printf("could not publish move: %v\n", err)
+				} else {
+					fmt.Printf("Move published successfully")
+				}
+
 			}
 		case "status":
 			gameState.CommandStatus()
